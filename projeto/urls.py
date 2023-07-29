ef630e25b9a0975f3b978b17d56671dce8e51ca4
Kleyton da Silva <nksaddam@gmail.com>
2023-07-29 17:32:01 -0300

    atualização urls e views django 29/07/23

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,18 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import  path, include
 
-def home(request):
-    return HttpResponse ('HOME!')
-def sobre(request):
-    return HttpResponse ('SOBRE!')
-def contato(request):
-    return HttpResponse ('CONTATO!')
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), #home
-    path('sobre/', sobre), #sobre
-    path('contato/', contato), #contato
+    path('',include('recipes.urls')),
 ]
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def my_view(request):
-    return HttpResponse ('Linda string de resposta!')
+
+def home(resquest):
+    return HttpResponse('Home 1')
+
+def sobre(request):
+    return HttpResponse ('SOBRE 1')
+
+def contato(request):
+    return HttpResponse ('CONTATO teste')
